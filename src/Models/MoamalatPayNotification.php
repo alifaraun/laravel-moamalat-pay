3fcb43d10ce38fbc2edc98868d4994d539e9d789
Ali Faraun (lenovo) <ali1996426@hotmail.com>
2022-08-25 00:43:12 +0200

    added scopes to model approved , verified and currentCredential

--- src/Models/MoamalatPayNotification.php
+++ src/Models/MoamalatPayNotification.php
@@ -47,4 +47,41 @@ class MoamalatPayNotification extends Model
         'request' => 'string',
         'verified' => 'string',
     ];
+
+
+
+    /**
+     * Scope a query to only include approved transactions.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @return void
+     */
+    public function scopeApproved($query)
+    {
+        $query->where('ActionCode', '00');
+    }
+
+    /**
+     * Scope a query to only include verified transactions.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @return void
+     */
+    public function scopeVerified($query)
+    {
+        $query->where('verified', 1);
+    }
+
+    /**
+     * Scope a query to only include transactions with current credentials terminal_id and mercahnt_id.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @return void
+     */
+    public function scopeCurrentCredential($query)
+    {
+        $query
+            ->where('MerchantId', config('moamalat-pay.merchant_id'))
+            ->where('TerminalId', config('moamalat-pay.terminal_id'));
+    }
 }
